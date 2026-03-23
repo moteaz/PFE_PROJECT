35c3d623c6acc21f2751cb569bd76980ea84e39d
Moetaz <motaz062002@gmail.com>
2026-03-23 16:46:30 +0100

    Backend Auth

--- backend/prisma/migrations/20260323153410_add_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "users_createdAt_idx";
-
--- DropIndex
-DROP INDEX "users_role_idx";
-
--- CreateIndex
-CREATE INDEX "users_role_isActive_createdAt_idx" ON "users"("role", "isActive", "createdAt");
